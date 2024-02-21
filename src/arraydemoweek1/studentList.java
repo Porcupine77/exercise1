e1a93e7944d3d59548bb637ac49657ea1856167b
Porcupine77 <156955247+Porcupine77@users.noreply.github.com>
2024-02-21 01:56:53 -0500

    Update studentList.java

--- src/arraydemoweek1/studentList.java
+++ src/arraydemoweek1/studentList.java
@@ -10,12 +10,13 @@ public class studentList
     {
         Student s1 = new Student(1, "Aman");
         
-        Student[] studentList = new Student[4];
+        Student[] studentList = new Student[5];
         
         studentList[0] = s1;
         studentList[1] = new Student(2, "Preet");
         studentList[2] = new Student(3, "Rai");
         studentList[3] = new Student(4, "hey");
+        studentList[4] = new Student(5, "hello");
         
         for(int i = 0; i<studentList.length; i++)
         {
