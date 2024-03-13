06e93418ea049f769ecc82b23bb73f04cdbae0fb
Porcupine77 <rai34@sheridancollege.ca>
2024-03-13 01:47:04 -0400

    Branch commit

---
+++ src/arraydemoweek1/PartTimeStudent.java
@@ -0,0 +1,24 @@
+package arraydemoweek1;
+
+/**
+ *
+ * @author amanp
+ */
+public class PartTimeStudent extends Student
+{
+
+    public PartTimeStudent(int sID, String sName)
+    {
+        super(sID, sName);
+    }
+    public int getNumCourses()
+    {
+        int numCourses = 0;
+        return numCourses;
+    }      
+    
+    public void setNumCourses(int numCourses)
+    {
+    
+    }        
+}
